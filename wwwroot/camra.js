463cf437de19b60f10c1c6b15d38c2d6d7c0ccba
santosh singh <santosh_singh2@dell.com>
2022-06-27 11:41:46 +0800

    Added webcam

---
+++ wwwroot/camra.js
@@ -0,0 +1,24 @@
+﻿
+export async function init(videoElementRef, dotnetObjectRef) {
+    console.log("Init");
+    try {
+        var stream = await navigator.mediaDevices.getUserMedia({ video: true });
+        onSuccess(stream, videoElementRef);
+        dotnetObjectRef.invokeMethodAsync("OnSuccess");
+    }
+    catch (e) {
+        onFailure(e, dotnetObjectRef)
+
+    }
+}
+function onSuccess(stream, videoElementRef) {
+
+    videoElementRef.srcObject = stream;
+    videoElementRef.play();
+}
+
+function onFailure(exception, dotnetObjectRef) {
+    console.log("Exception occurred", exception);
+    dotnetObjectRef.invokeMethodAsync("onFailure", exception.message);
+}
+
